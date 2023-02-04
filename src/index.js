f9c879c332ef9869a24ef18b324935c43dee1296
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-04 12:43:58 +0200

    Setting up server start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const config = require('./config');
 
 
 const app = express();
+
+
+app.listen(config.PORT, ()=> console.log(`Server is running on port ${config.PORT}`));
